599df8ac7940229503a227887a4045c510d88a37
dwrtaza2 <dwrtaza2@csu.fullerton.edu>
2024-09-22 21:06:07 -0700

    First commit

--- fib.c
+++ fib.c
@@ -1,7 +1,98 @@
+// Name: Dominic Wrtaza
+// Date: 9/20/2024
+// File Purpose: To perfrom the Fibonacci sequence
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[]) {
-   printf("Hello!\n");
-   return 0;
+#define MAX_SIZE 1000  
+
+
+unsigned long int fib_i_core(unsigned long int n);
+unsigned long int fib_r_core(unsigned long int n);
+
+unsigned long int memo[MAX_SIZE];
+
+void init_memo() {
+    for (unsigned long int i = 0; i < MAX_SIZE; i++) {
+        memo[i] = 0;  
+    }
+}
+
+
+unsigned long int fibonacci_iterative(unsigned long int n) {
+    if (memo[n] != 0) {
+        return memo[n];
+    }
+    unsigned long int result = fib_i_core(n);
+    memo[n] = result;
+    return result;
+}
+
+
+unsigned long int fibonacci_recursive(unsigned long int n) {
+    if (memo[n] != 0) {
+        return memo[n];
+    }
+    unsigned long int result = fib_r_core(n);
+    memo[n] = result;
+    return result;
+}
+
+
+unsigned long int fib_i_core(unsigned long int n) {
+    if (n <= 1) {
+        return n;
+    }
+    unsigned long int a = 0, b = 1, temp;
+    for (unsigned long int i = 2; i <= n; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
+}
+
+
+unsigned long int fib_r_core(unsigned long int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fib_r_core(n - 1) + fib_r_core(n - 2);
+}
+
+int main(int argc, char *argv[]) {
+
+    unsigned long int first_num = atoi(argv[1]);
+
+    FILE *file = fopen(argv[3], "r");
+    if (!file) {
+        printf("Failed to open file: %s\n", argv[3]);
+        return 1;
+    }
+    
+    unsigned long int file_num;
+    fscanf(file, "%ld", &file_num);
+    fclose(file);
+
+    unsigned long int sum_of_firstNum_fileNum = first_num + file_num - 1;
+
+    if (sum_of_firstNum_fileNum >= MAX_SIZE) {
+        return 1;
+    }
+
+
+    init_memo();
+
+    unsigned long int result;
+    if (argv[2][0] == 'r') {
+        result = fibonacci_recursive(sum_of_firstNum_fileNum);
+    } else if (argv[2][0] == 'i') {
+        result = fibonacci_iterative(sum_of_firstNum_fileNum);
+    } else {
+        return 1;
+    }
+
+    printf("%ld\n", result);
+
+    return 0;
 }

a740afb85195204d1264dd0d4381086b0bb7f917
dwrtaza2 <dwrtaza2@csu.fullerton.edu>
2024-09-22 21:23:02 -0700

    added size and complied

--- fib.c
+++ fib.c
@@ -13,86 +13,69 @@ unsigned long int fib_r_core(unsigned long int n);
 unsigned long int memo[MAX_SIZE];
 
 void init_memo() {
-    for (unsigned long int i = 0; i < MAX_SIZE; i++) {
-        memo[i] = 0;  
-    }
+   for (unsigned long int i = 0; i < MAX_SIZE; i++) {
+    memo[i] = 0;  
+   }
 }
 
 
 unsigned long int fibonacci_iterative(unsigned long int n) {
     if (memo[n] != 0) {
-        return memo[n];
+      return memo[n];
     }
-    unsigned long int result = fib_i_core(n);
-    memo[n] = result;
-    return result;
+   unsigned long int result = fib_i_core(n);
+   memo[n] = result;
+   return result;
 }
 
 
 unsigned long int fibonacci_recursive(unsigned long int n) {
-    if (memo[n] != 0) {
-        return memo[n];
-    }
-    unsigned long int result = fib_r_core(n);
-    memo[n] = result;
-    return result;
+   if (memo[n] != 0) {
+      return memo[n];
+   }
+   unsigned long int result = fib_r_core(n);
+   memo[n] = result;
+   return result;
 }
 
 
 unsigned long int fib_i_core(unsigned long int n) {
-    if (n <= 1) {
-        return n;
-    }
-    unsigned long int a = 0, b = 1, temp;
-    for (unsigned long int i = 2; i <= n; i++) {
-        temp = a + b;
-        a = b;
-        b = temp;
-    }
-    return b;
+   if (n <= 1) {
+      return n;
+   }
+   unsigned long int a = 0, b = 1, temp;
+   for (unsigned long int i = 2; i <= n; i++) {
+      temp = a + b;
+      a = b;
+      b = temp;
+   }
+   return b;
 }
 
 
 unsigned long int fib_r_core(unsigned long int n) {
     if (n <= 1) {
-        return n;
+      return n;
     }
     return fib_r_core(n - 1) + fib_r_core(n - 2);
 }
-
 int main(int argc, char *argv[]) {
+ 
 
-    unsigned long int first_num = atoi(argv[1]);
-
-    FILE *file = fopen(argv[3], "r");
-    if (!file) {
-        printf("Failed to open file: %s\n", argv[3]);
-        return 1;
-    }
-    
-    unsigned long int file_num;
-    fscanf(file, "%ld", &file_num);
-    fclose(file);
-
-    unsigned long int sum_of_firstNum_fileNum = first_num + file_num - 1;
-
-    if (sum_of_firstNum_fileNum >= MAX_SIZE) {
-        return 1;
-    }
+   unsigned long int first_num = atoi(argv[1]);
+   unsigned long int num_sum = first_num -1;
 
 
-    init_memo();
+   init_memo();
 
-    unsigned long int result;
-    if (argv[2][0] == 'r') {
-        result = fibonacci_recursive(sum_of_firstNum_fileNum);
-    } else if (argv[2][0] == 'i') {
-        result = fibonacci_iterative(sum_of_firstNum_fileNum);
-    } else {
-        return 1;
-    }
+   unsigned long int result;
+   if (argv[2][0] == 'r') {
+      result = fibonacci_recursive(num_sum);
+   } else if (argv[2][0] == 'i') {
+      result = fibonacci_iterative(num_sum);
+   }
 
-    printf("%ld\n", result);
+   printf("%ld\n", result);
 
-    return 0;
+   return 0;
 }
